7e6786d1f1b94b9daeb4f3192696ceb56c8109e7
Genaro Almaraz <jg08950@gmail.com>
2019-07-02 13:42:54 -0500

    Remove Consonants From String (python code)

---
+++ RemoveConsonantsFromString.py
@@ -0,0 +1,25 @@
+T = int(input())
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+for i in range(T):
+    phrase = str(input())
+
+    res = list()
+
+    noVowel = True
+
+    for j in phrase:
+        if j in vowels:
+            res.append(j)
+            noVowel = False
+        elif j is " ":
+            res.append(' ')
+
+    if noVowel == True:
+        print("No Vowel", end = "")
+    else:
+        for k in res:
+            print(k, end = "")
+
+    print()
