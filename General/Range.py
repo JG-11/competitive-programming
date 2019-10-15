9a0cfca0b4746d263d2f6e7dbe4cd07d78a3f2aa
Genaro Almaraz <jg08950@gmail.com>
2019-10-15 17:48:56 -0500

    Practicing range() function syntax and arguments

---
+++ General/Range.py
@@ -0,0 +1,15 @@
+#range(start, stop, step)
+
+print("range(5)")
+for i in range(5):
+    print(i)
+
+
+print("range(5, 11)")
+for i in range(5, 11):
+    print(i)
+
+
+print("range(5, 11, 2)")
+for i in range(5, 11, 2):
+    print(i)
